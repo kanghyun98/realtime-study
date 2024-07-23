4d3bdaace846b752feafaa52001f9d7282091421
kanghyun98 <lkh981107@gmail.com>
2024-07-24 00:01:34 +0900

    add basic web socket code

--- src/client/app.js
+++ src/client/app.js
@@ -1,1 +1,17 @@
-hello;
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('connected to the server ✅');
+});
+
+socket.addEventListener('close', () => {
+  console.log('disconnected from the server ❌');
+});
+
+socket.addEventListener('message', (message) => {
+  console.log('server message: ', message.data);
+});
+
+setTimeout(() => {
+  socket.send('nice to meet you, server!');
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -10,4 +12,21 @@ app.use('/client', express.static(__dirname + '/client'));
 app.get('/', (req, res) => res.render('home'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 같은 서버에 http, webSocket 둘 다 작동시키는 방법
+const server = http.createServer(app); // http server
+const wss = new WebSocket.Server({ server }); // webSocket server
+
+wss.on('connection', (socket) => {
+  console.log('connected to the client ✅');
+
+  socket.on('close', () => console.log('disconnected from the client ❌'));
+
+  socket.on('message', (message) =>
+    console.log('client message: ', message.toString())
+  );
+
+  socket.send('hello client~~~');
+});
+
+server.listen(3000, handleListen);
